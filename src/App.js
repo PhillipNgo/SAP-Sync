9cc90ac3dca57bab0c363ed983d225446ab30c93
Chan, Justin <Justin.Chan@concur.com>
2017-07-18 10:44:16 -0400

    imported styles.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import './../styles/myStyles.less';
-
-const primaryColor = '#2379BF';
+import styles from '../styles/myStyles';
 
 export default class App extends React.Component {
   render() {
     return (
       <View>
-        <View style={{width:'100%', height:'5%', backgroundColor:primaryColor}} />
-        <View style={{width:'100%', height:'10%', backgroundColor:primaryColor}} />
+        <View style={styles['status-bar']} />
+        <View style={styles['status-bar']} />
       </View>
     );
   }
---
+++ styles/myStyles.js
@@ -0,0 +1,9 @@
+const styles = {
+  'status-bar': {
+    backgroundColor: '#2379BF',
+    width:'100%',
+    height:'7.8%'
+  }
+}
+
+export default styles;
