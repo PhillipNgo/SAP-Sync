704a55bbf55aa57be7f4495f2809c2c1960e3bfb
Chan, Justin <Justin.Chan@concur.com>
2017-07-18 12:17:45 -0400

    Simple first page, styles update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, AppRegistry, TextInput  } from 'react-native';
 import styles from '../styles/myStyles';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: 'Useless Placeholder' };
+  }
+
   render() {
     return (
       <View>
-        <View style={styles['status-bar']} />
-        <View style={styles['status-bar']} />
+        <View style={styles.statusBar} />
+        <View style={styles.tabBar}>
+          <Text style={[styles.headingOne, {textAlign: 'center'}]}>
+            Hello
+          </Text>
+        </View>
+
+        <View style={styles.loginBlock}>
+          <TextInput
+            style={[styles.loginInput, styles.centerBlock]}
+            onChangeText={(text) => this.setState({text})}
+            placeholder='Username'
+          />
+          <TextInput
+            style={[styles.loginInput, styles.centerBlock]}
+            onChangeText={(text) => this.setState({text})}
+            placeholder='Password'
+          />
+        </View>
       </View>
     );
   }
--- styles/myStyles.js
+++ styles/myStyles.js
@@ -1,9 +1,43 @@
-const styles = {
-  'status-bar': {
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+  'centerBlock': {
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  },
+  'headingOne': {
+    fontSize: 40
+  },
+  'statusBar': {
     backgroundColor: '#2379BF',
     width:'100%',
-    height:'7.8%'
+    height:'7%'
+  },
+  'tabBar': {
+    backgroundColor: '#2379BF',
+    width:'100%',
+    height:'28%'
+  },
+  'loginBlock': {
+    marginTop: 'auto'
+  },
+  'loginInput': {
+    textAlign:'center',
+    borderWidth:1,
+    borderColor: 'gray',
+    height: 40,
+    width: '50%',
+    borderRadius: 8,
+    marginBottom: 10
   }
-}
+});
 
 export default styles;
